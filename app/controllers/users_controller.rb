6f4ff1e91a18b3b982dbd92ed04c8f203dcd0127
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-10-05 11:56:43 -0700

    Signup is working. User profile is up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+		render :new
+	end
+
+	def create
+		user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+		@user = User.create(user_params)
+
+		redirect_to "/users/#{@user.id}"
+	end
+
+	def show
+		@user = User.find_by({id: params[:id]})
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  
+
+  get '/users/new', to: 'users#new' 
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
 end
