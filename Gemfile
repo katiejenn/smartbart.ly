5250ada3177fc4a5921091c379e1fbed04c58cbe
inei <iancnei@gmail.com>
2015-10-05 15:12:51 -0700

    install HTTParty and begin work on querying API, currently trying to get routes from a specified station

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Use HTTParty for API requests
+gem 'httparty'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
 
 	def index
-		
+		apiData = BartAPI.new()
+		@station = apiData.station("stninfo", {orig: "mlbr"})
+		# @data = apiData.station("stns", nil)
+		# @data = apiData.schedule("routesched", {route: 1})		
+		# @data = apiData.route("routes", nil)
+		# render json: @data
 	end
 end
