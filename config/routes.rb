06abee47c14caa15eb9959d67a49bf9ea8a4e3d4
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-10-05 12:08:02 -0700

    Login working

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new' 
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show'
+
+  get '/sign_in', to:'sessions#new'
+  post "/sessions", to: "sessions#create"
 end
