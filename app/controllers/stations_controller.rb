652de4d95bd312ac6a059bb734efd0bf81358a85
inei <iancnei@gmail.com>
2015-10-05 20:13:44 -0700

    refactor station arrival time code to work with any station, change station show page to display each station's arrival times for each route, ran a schema migration (this'll probably cause a merge conflict ._. )

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -8,10 +8,10 @@ class StationsController < ApplicationController
   	@stationRoutes = []
   	@stationTimes = {}
 
-  	wantedStation = Station.find_by_id(params[:id])
+  	@wantedStation = Station.find_by_id(params[:id])
 
   	# get the specified station's info, can replace mlbr with info from the params later on
-  	station = apiData.station("stninfo", {orig: wantedStation.abbreviation})
+  	station = apiData.station("stninfo", {orig: @wantedStation.abbreviation})
 
   	# drill down to the routes we need and append them to the array of routes
   	getRoutes(station, "north_routes")
@@ -23,7 +23,7 @@ class StationsController < ApplicationController
   		routeSchedule = apiData.schedule("routesched", {route: route})
   		routeSchedule["root"]["route"]["train"].each do |train|
   			train["stop"].each do |stop|
-  				if stop["station"].downcase == wantedStation.abbreviation && stop["origTime"] != nil
+  				if stop["station"].downcase == @wantedStation.abbreviation && stop["origTime"] != nil
   					@stationTimes["#{route}"].push(stop["origTime"])
   					break
   				end
