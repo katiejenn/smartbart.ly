11f491cedf08fba25785584867d61da556ffcbbe
inei <iancnei@gmail.com>
2015-10-06 17:29:57 -0700

    create lines model & controller, create lines index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'lines/index'
+
+  get 'lines/show'
+
  root 'stations#index'
 
   get '/sign_in', to:'sessions#new'
   post "/sessions", to: "sessions#create"
 
-  resources :stations, :users
-
   post '/tips', to: "tips#create"
+
+  resources :stations, :users, :lines
 
 end
   
